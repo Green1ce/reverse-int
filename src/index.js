ecd3a466ca36f78f6ca036d7c9cad42660336021
Green1ce <cha1iiep@mail.ru>
2021-03-02 22:27:47 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) n *= -1;
+    let str = n.toString();
+    let answer = [];
+    for (let i = 0; i < str.length; i++) {
+        answer.unshift(str[i]);
+    }
+    answer = answer.join('');
+    return answer;
 }
